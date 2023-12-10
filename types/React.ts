2fce19d4d096da16202d308d7e5f3f113a966776
Natan Lifshitz <natan.lifshitz@caesarealabs.com>
2023-12-10 18:49:58 +0200

    0.2.1

--- types/React.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-/**
- * Typescript utility for defining a "children" argument type in components
- */
-export type ReactComponent = React.JSX.Element | React.JSX.Element[] | string
--- types/TypeUtils.ts
+++
@@ -1,3 +0,0 @@
-export function check<T>(value: T): T {
-    return value
-}
